02a6d1ca01101cfccc72232f511f053e01ec0ac1
work <1390095882@qq.com>
2021-02-24 09:09:21 +0800

    PaintedCat@20210224

--- js/contentsList.js
+++ js/contentsList.js
@@ -4,4 +4,5 @@ var content = [
 {url:"project/word/WebProduction.html",title:"涉及网页制作的基础问题",family:"2020.10.13",familyID:0},
 {url:"project/canvas/drawNine.html",title:"Canvas绘制九角星",family:"2020.10.14",familyID:3},
 {url:"project/GameFlyBird/index.html",title:"像素鸟小游戏",family:"2020.10.18",familyID:3},
+{url:"project/canvas/drawClock.html",title:"Canvas绘制钟表",family:"2021.02.24",familyID:3},
 ]
